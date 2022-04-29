8bddc7c9c20f60b15b8c11b16245458994747842
valentino spada <valenspada@hotmail.com>
2022-04-29 00:56:58 -0300

    bug fixing titles on scroll animation 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,24 +12,22 @@ import image from './Images/curriculum img.jpg'
 
 function About() {
     return (
-        <Stack bg='#222639'  color='#F2F2F2' id='about' minHeight={{ base: '1600px', sm: '1500px', md: '1300px', lg: '100vh' }} fontFamily='"Red Hat Display", sans-serif' direction={{ base: 'column', sm: 'column', md: 'column', lg: 'row' }} alignItems={'center'} justifyContent={'space-evenly'}>
+        <Stack bg='#222639'  color='#F2F2F2' id='about' minHeight={{ base: '1300px', sm: '1250px', md: '1300px', lg: '100vh' }} fontFamily='"Red Hat Display", sans-serif' direction={{ base: 'column', sm: 'column', md: 'column', lg: 'row' }} alignItems={'center'} justifyContent={{md:'space-evenly' ,lg: 'space-evenly'}}>
             <Stack pl={{ base: '0', sm: '0', md: '0', lg: '123px' }} alignItems={'center'} justifyContent={'center'}>
                 <Stack mt={{ base: '110px', sm: '110px', md: '40px', lg: '0' }}>
                     <Text fontSize='6xl' pr={{ base: '0', sm: '0', md: '0', lg: '55px' }}>Sobre mi</Text>
                 </Stack>
-                <Stack spacing={10}>
-                    <Stack pl={14}>
-                        <Stack h='350px' position={'absolute'} display={{ base: 'none', sm: 'none', md: 'none', lg: 'flex' }} w='355px' border={'solid 5px'} borderColor='#5F527A' borderTopRightRadius={'5px'} borderLeft={'none'} borderBottom={'none'}>
+                <Stack spacing={10} >
+                    <Stack pl={14} >
+                        <Stack h='350px'  position={'absolute'} display={{ base: 'none', sm: 'none', md: 'none', lg: 'flex' }} w='355px' border={'solid 5px'} borderColor='#5F527A' borderTopRightRadius={'5px'} borderLeft={'none'} borderBottom={'none'}>
                         </Stack>
                     </Stack>
-                    <Stack alignItems={'center'} justifyContent={'center'}>
+                    <Stack alignItems={'center'} justifyContent={'center'} >
                         <Image src={image} h={{ base: '200px', sm: '270px', md: '300px', lg: '400px' }} borderRadius={5} border='solid 5px' borderColor={'#5F527A'}></Image>
                     </Stack>
                 </Stack>
             </Stack>
-            <Stack initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: true }} direction={'column'} w={{ base: '90%', sm: '85%', md: '70%', lg: '40%' }} bg='#5F527A' alignItems={'center'} borderRadius={'20px'} minHeight='540px' pt={'70px'} paddingLeft={{base: '10px', sm: '30px', md: '50px', lg: '70px'}} mb={'200px'} paddingRight={{base: '10px', sm: '30px', md: '50px', lg: '70px'}}>
+            <Stack direction={'column'} textAlign={{base: 'center', sm: 'center', md:'center', lg:'initial'}} w={{ base: '90%', sm: '85%', md: '70%', lg: '40%' }} bg='#5F527A' alignItems={'center'} borderRadius={'20px'} minHeight='540px' pt={{base: '10px', sm: '10px', md: '20px', lg: '70px'}} paddingLeft={{base: '10px', sm: '30px', md: '50px', lg: '70px'}} mb={'200px'} paddingRight={{base: '10px', sm: '30px', md: '50px', lg: '70px'}}>
 
                 <Text fontSize='xl'>
                     Soy un desarrollador Full Stack con orientacion al Front-end
@@ -45,7 +43,7 @@ function About() {
                     Cuento con habilidades de trabajo en equipo y liderazgo.
                     Me concidero una persona creativa, productiva, adaptable y sociable.
                 </Text>
-                <Stack pt={'90px'} w='200px'>
+                <Stack pt={'90px'} w='200px' margin={'auto'}>
                     <a href='https://drive.google.com/file/d/1QmJxa6gGmfuXGcbNPyi81irC1lkqAGej/view' target={'_blank'}><Button variant='outline' marginBottom={'30px'} _hover={{ color: '#262626', bg: 'white' }} fontSize='xl' size='md'>Descargar CV</Button></a>
                 </Stack>
             </Stack>
